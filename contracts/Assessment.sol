851a4ee4523512eb71bb1cfcb6ccbfeb0aa12f97
chintu4564 <151220493+chintu4564@users.noreply.github.com>
2023-12-01 11:50:03 +0530

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,18 +1,21 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-//import "hardhat/console.sol";
-
 contract Assessment {
     address payable public owner;
     uint256 public balance;
+    uint public pin;
 
     event Deposit(uint256 amount);
     event Withdraw(uint256 amount);
+    event PinSet(uint256 newPin);
+    event OddCubeResult(uint256 input, uint256 result);
+    event SelfDestruct(address indexed beneficiary, uint256 remainingBalance);
 
     constructor(uint initBalance) payable {
         owner = payable(msg.sender);
         balance = initBalance;
+        pin = 1001;
     }
 
     function getBalance() public view returns(uint256){
@@ -21,21 +24,12 @@ contract Assessment {
 
     function deposit(uint256 _amount) public payable {
         uint _previousBalance = balance;
-
-        // make sure this is the owner
         require(msg.sender == owner, "You are not the owner of this account");
-
-        // perform transaction
         balance += _amount;
-
-        // assert transaction completed successfully
         assert(balance == _previousBalance + _amount);
-
-        // emit the event
         emit Deposit(_amount);
     }
 
-    // custom error
     error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
 
     function withdraw(uint256 _withdrawAmount) public {
@@ -47,14 +41,28 @@ contract Assessment {
                 withdrawAmount: _withdrawAmount
             });
         }
-
-        // withdraw the given amount
         balance -= _withdrawAmount;
-
-        // assert the balance is correct
         assert(balance == (_previousBalance - _withdrawAmount));
-
-        // emit the event
         emit Withdraw(_withdrawAmount);
+    }
+
+    function setPin(uint _newPin) public {
+        require(msg.sender == owner, "You are not the owner of this account");
+        pin = _newPin;
+        emit PinSet(_newPin);
+    }
+
+    function oddCube(uint256 _input) public {
+        require(msg.sender == owner, "You are not the owner of this account");
+        require(_input % 2 != 0, "Input must be an odd number");
+        uint256 result = _input * _input * _input;
+        emit OddCubeResult(_input, result);
+    }
+
+    function selfDestruct(address payable _beneficiary) public {
+        require(msg.sender == owner, "You are not the owner of this account");
+        _beneficiary.transfer(balance);
+        emit SelfDestruct(_beneficiary, balance);
+        selfDestruct(_beneficiary);
     }
 }
